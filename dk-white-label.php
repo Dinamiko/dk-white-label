2a6f40aa518848cd27accbc54c8d0ec5e98cafa4
Emili Castells <emili.castells@dinamiko.com>
2016-04-22 19:32:44 +0200

    version 0.2

--- dk-white-label.php
+++ dk-white-label.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: DK White Label
- * Version: 0.1
+ * Version: 0.2
  * Plugin URI: 
  * Description: White Label WordPress Admin 
  * Author: Emili Castells
@@ -47,7 +47,7 @@ if ( ! class_exists( 'DKWL' ) ) {
 
 		private function setup_constants() {
 
-			if ( ! defined( 'DKWL_VERSION' ) ) { define( 'DKWL_VERSION', '0.1' ); }
+			if ( ! defined( 'DKWL_VERSION' ) ) { define( 'DKWL_VERSION', '0.2' ); }
 			if ( ! defined( 'DKWL_PLUGIN_DIR' ) ) { define( 'DKWL_PLUGIN_DIR', plugin_dir_path( __FILE__ ) ); }
 			if ( ! defined( 'DKWL_PLUGIN_URL' ) ) { define( 'DKWLPLUGIN_URL', plugin_dir_url( __FILE__ ) ); }
 			if ( ! defined( 'DKWL_PLUGIN_FILE' ) ) { define( 'DKWL_PLUGIN_FILE', __FILE__ ); }			
